f07539c300437b5eecff26ab5c24e32fad48fae4
Pedro Sanchez <pedrolampo50@gmail.com>
2024-06-15 15:56:16 -0300

    steam api connected

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9001;
+
+const games = [];
+
+// Cors configuration - Allows requests from localhost:4200
+const corsOptions = {
+  origin: ['*', 'http://localhost:3000'],
+  optionsSuccessStatus: 204,
+  methods: 'GET, POST, PUT, DELETE',
+};
+
+// fetch(
+//   'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FC247331076EB69673B6D48C88A6D979&steamids=76561198038738480'
+// )
+//   .then((res) => res.json())
+//   .then((data) => console.log(data.response.players));
+
+// Use cors middleware
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+  let message = 'Try /games for all of the apps';
+
+  res.send(JSON.parse(message));
+});
+
+app.get('/games', (req, res) => {
+  fetch('http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json')
+    .then((res) => res.json())
+    .then((data) => {
+      data.applist.apps.forEach((game) => games.push(game));
+    });
+
+  res.send(games);
+});
+
+app.get('/games/:id', (req, res) => {
+  const id = req.params.id;
+
+  fetch(`http://store.steampowered.com/api/appdetails?appids=${id}&cc=US`)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send(data[id].data);
+    });
 });
 
 app.listen(port, () => {
